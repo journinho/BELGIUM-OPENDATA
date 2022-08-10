1780d8461704ac76cffa693ba8427d140991fff5
Amra Dorjbayar <47465320+journinho@users.noreply.github.com>
2022-08-10 13:21:08 +0200

    Create death_per_day.py

---
+++ death_per_day.py
@@ -0,0 +1,5 @@
+import pandas as pd
+df = pd.read_csv('https://statbel.fgov.be/sites/default/files/files/opendata/deathday/DEMO_DEATH_OPEN.zip',sep='|')
+df['DATE'] = pd.to_datetime(df['DT_DATE'], dayfirst = True)
+sterfte_per_dag = pd.pivot_table(df, index = 'DATE', values = ['MS_NUM_DEATH'], aggfunc='sum')
+sterfte_per_dag.to_csv('sterfte_per_dag.csv', index=False)
